b52d2d370488ba47c75653b9e1dad610863bbea7
roncewind <roncewind@users.noreply.github.com>
2023-07-31 19:35:15 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.0.2

--- cmd/github.go
+++ cmd/github.go
@@ -1,11 +1,13 @@
-// 0.2.1
-// Created by make-go-github-file.yaml on Fri Mar 31 21:16:54 UTC 2023
+// 0.0.2
+// Created by make-go-github-file.yaml on Mon Jul 31 19:35:15 UTC 2023
 package cmd
 
-var githubDate string = "2023-03-31"
-var githubIteration string = "0"
-var githubRef string = "refs/tags/0.2.1"
-var githubRefName string = "0.2.1"
-var githubRepository string = "Senzing/validate"
-var githubRepositoryName string = "validate"
-var githubVersion string = "0.2.1"
+var (
+	githubDate           string = "2023-07-31"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.0.2"
+	githubRefName        string = "0.0.2"
+	githubRepository     string = "Senzing/validate"
+	githubRepositoryName string = "validate"
+	githubVersion        string = "0.0.2"
+)
