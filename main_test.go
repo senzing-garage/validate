d0e4c89a736cc5bbba2f1c0cb25fba5aa797187e
Michael Dockter <michael@dockter.com>
2024-07-02 13:35:08 -0400

    #81 Prepare for versioned release

--- main_test.go
+++ main_test.go
@@ -3,34 +3,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 /*
  * The unit tests in this file simulate command line invocation.
  */
 func TestMain(test *testing.T) {
-	tempDir := test.TempDir()
-	inputFile := filepath.Join(tempDir, "move-main-input.jsonl")
-	err := touchFile(inputFile)
-	require.NoError(test, err)
-	os.Setenv("SENZING_TOOLS_INPUT_URL", fmt.Sprintf("file://%s", inputFile))
+	_ = test
+	os.Args = []string{"command-name", "--version"}
 	main()
-}
-
-// ----------------------------------------------------------------------------
-// Utiity functions
-// ----------------------------------------------------------------------------
-
-func touchFile(name string) error {
-	file, err := os.OpenFile(name, os.O_RDONLY|os.O_CREATE, 0644)
-	if err != nil {
-		return err
-	}
-	return file.Close()
 }
